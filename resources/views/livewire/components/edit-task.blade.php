2d12cef30e2cea290a33424e4fc31ff490f7e871
ainvention <alexinvention@gmail.com>
2021-02-17 01:58:40 +0100

    commit error fix

--- resources/views/livewire/components/edit-task.blade.php
+++ resources/views/livewire/components/edit-task.blade.php
@@ -139,13 +139,8 @@
             <div class="flex flex-col md:flex-row w-full justify-evenly py-1 border-gray-200 border-b-2">
                 <div class="flex w-1/3 text-gray-500 text-left">Weight(Kg)</div>
                 <div class="flex flex-row w-full text-gray-500 ml-0">
-<<<<<<< HEAD
-                    <input wire:model="weight"
-                        class="w-full text-gray-400 text-left border-2 border-green-500 rounded-md"
-=======
                     <input wire:model="weight" type="number"
                         class="w-full text-gray-400 text-left border-2 {{ empty($weight) ? 'bg-green-100 border-2 border-green-500 rounded-md' : '' }}"
->>>>>>> dev
                         placeholder="type item weights" />
                 </div>
             </div>
