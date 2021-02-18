6dd30be314e1d2fa7c081d703cc5db7af243d108
ainvention <alexinvention@gmail.com>
2021-02-18 02:26:16 +0100

    forced https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \URL::forceRootUrl(\Config::get('app.url'));
+        if (str_contains(\Config::get('app.url'), 'https://')) {
+            \URL::forceScheme('https');
+        }
+
         Blade::directive('icon', function ($expression) {
             return "<i class=\"fas fa-fw fa-{{ $expression }}\"></i>";
         });
