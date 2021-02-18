31905d4d0fddf6289713b06bb549716a3ebf8533
ainvention <alexinvention@gmail.com>
2021-02-18 14:04:10 +0100

    savePhoto() fix

--- app/Http/Livewire/Sending/Step1.php
+++ app/Http/Livewire/Sending/Step1.php
@@ -338,7 +338,7 @@ class Step1 extends Component
             ]
         );
 
-        $path = $this->photo->store('public');
+        $path = $this->photo->store('sendings', 'public');
 
         if (!file_exists($path)) {
             File::makeDirectory($path, $mode = 0755, true, true);
