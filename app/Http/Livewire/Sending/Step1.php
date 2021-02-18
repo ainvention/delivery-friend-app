75fc47ab1ab1d5d092350255c30ff455bafd6d25
ainvention <alexinvention@gmail.com>
2021-02-18 11:25:30 +0100

    savePhoto() fix

--- app/Http/Livewire/Sending/Step1.php
+++ app/Http/Livewire/Sending/Step1.php
@@ -338,7 +338,7 @@ class Step1 extends Component
             ]
         );
 
-        $path = $this->photo->store('sendings', $this->photo);
+        $path = $this->photo->store('sendings');
 
         $this->photo = $path;
 
