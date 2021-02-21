4721e526d88d04056ab55b8cb0aca05cfeaf4fff
ainvention <alexinvention@gmail.com>
2021-02-21 03:57:03 +0100

    profile-photos storage link added

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,6 +67,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('storage/profile-photos') => storage_path('app/public/profile-photos'),
     ],
 
 ];
