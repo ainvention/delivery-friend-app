adb2223cff9dbb6259a684b5c01bba50704e28b6
ainvention <alexinvention@gmail.com>
2021-02-27 23:29:16 +0100

    loading state css adjusted

--- resources/views/livewire/search/step1.blade.php
+++ resources/views/livewire/search/step1.blade.php
@@ -6,7 +6,7 @@
         {{-- main page start --}}
         <div
             class="flex flex-col text-center mb-5 text-4xl item-center justify-center px-4 dark:text-white rounded-full leading-relaxed font-semibold tracking-wide">
-            Our customer needs your help!
+            Help us & reduce your opportunity costs!
             </h2>
             <span class="text-lg">or <a href="/sending" class="text-blue-500">
                     to send a item
