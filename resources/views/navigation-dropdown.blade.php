ec899b6aa4647496962047dd7326e04c10c213d4
ainvention <alexinvention@gmail.com>
2021-03-01 03:39:09 +0100

    profile image https fix: hard coded

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -32,8 +32,7 @@
                         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                         <button
                             class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-                            <img class="h-8 w-8 rounded-full object-cover"
-                                src="{{ 'https://delivery.jswizard.no/storage/'.Auth::user()->profile_photo_url  }}"
+                            <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url  }}"
                                 alt="{{ Auth::user()->name }}" />
                         </button>
                         @else
